612876ded63b7c03e725df47bacefc97aa83b8b7
Emre Sen <maemresen@yazilim.vip>
2020-12-31 20:52:02 +0300

    fix: small bug fixes

--- src/main/kotlin/vip/yazilim/springvip/templates/kotlin/entity/Person.kt
+++ src/main/kotlin/vip/yazilim/springvip/templates/kotlin/entity/Person.kt
@@ -1,8 +1,20 @@
 package vip.yazilim.springvip.templates.kotlin.entity
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
 /**
  *
  * @author maemresen - maemresen@yazilim.vip
  * 12/30/2020
  */
-class Person(val id: Long, val name: String, val age: Double)
+@Entity
+class Person(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+    val name: String,
+    val age: Double
+)
--- src/main/kotlin/vip/yazilim/springvip/templates/kotlin/rest/RestPerson.kt
+++ src/main/kotlin/vip/yazilim/springvip/templates/kotlin/rest/RestPerson.kt
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import vip.yazilim.libs.springvip.config.bean.IRestResponseBuilder
 import vip.yazilim.libs.springvip.util.generic.rest.AGenericRest
+import vip.yazilim.libs.springvip.util.generic.rest.GenericRest
+import vip.yazilim.libs.springvip.util.generic.rest.method.GetAll
 import vip.yazilim.libs.springvip.util.generic.service.IGenericServiceCrud
 import vip.yazilim.springvip.templates.kotlin.entity.Person
 import vip.yazilim.springvip.templates.kotlin.service.IPersonService
@@ -16,10 +18,11 @@ import kotlin.reflect.KClass
  */
 @RestController
 @RequestMapping("/person")
+@GenericRest
+@GetAll
 class RestPerson(
     restResponseBuilder: IRestResponseBuilder,
     personService: IPersonService,
 ) : AGenericRest<Person, Long>(restResponseBuilder, personService, Person::class, Long::class) {
-
 
 }
